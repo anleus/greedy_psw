e62a37314fe1a60fabc775bfbb7af6ce1304e152
crigasro <gasparrozalen@gmail.com>
2019-04-28 20:25:19 +0200

    Animaciones hechas. Movimiento mejorado

---
+++ greedy_game/Assets/Scripts/Character.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    [SerializeField] private float speed = 2f;
+    protected Vector2 direction;
+
+    protected Animator animator;
+
+    void Start()
+    {
+        
+    }
+
+    protected virtual void Update()
+    {
+        Move();
+    }
+
+    protected void Move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+        AnimCharacter(direction);
+    }
+
+    public void AnimCharacter(Vector2 direction) 
+    {
+        animator.SetFloat("x", direction.x);
+        animator.SetFloat("y", direction.y);        
+        animator.SetBool("stop", (direction.x == 0f && direction.y == 0f));        
+    }
+}
---
+++ greedy_game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Character
+{
+    void Start () 
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    protected override void Update()
+    {
+        GetInput();
+        base.Update();
+    }
+
+    private void GetInput()
+    {
+        direction = Vector2.zero;
+        // Arriba
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            direction += Vector2.up;
+        }
+
+        // Abajo
+        if(Input.GetKey(KeyCode.S)  || Input.GetKey(KeyCode.DownArrow))
+        {
+            direction += Vector2.down;
+        }
+
+        // Izquierda
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction += Vector2.left;
+        }
+
+        // Derecha
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            direction += Vector2.right;
+        }
+    }
+
+
+}
