3da3d9657c59d2fd7c9f4830ea85b1fe83768ad1
FVJohnny <jofervac@gmail.com>
2019-04-29 21:10:57 +0200

    Movimiento y colisiones menos buggy, sorting layers, prefab sombra

---
+++ greedy_game/Assets/Scripts/Characters/Shadow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shadow : MonoBehaviour
+{
+    public Vector3 offset = new Vector3(0f, -1f);
+
+
+    GameObject shadow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localPosition = offset;
+        transform.localRotation = Quaternion.identity;
+
+        SpriteRenderer parentRenderer = GetComponentInParent<SpriteRenderer>();
+        SpriteRenderer shadowRenderer = GetComponent<SpriteRenderer>();
+
+        shadowRenderer.sortingLayerName = parentRenderer.sortingLayerName;
+        shadowRenderer.sortingOrder = parentRenderer.sortingOrder - 1;
+
+        Debug.Log(shadowRenderer.sortingOrder);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.localPosition = offset;
+    }
+}
