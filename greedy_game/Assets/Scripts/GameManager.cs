88d7931795b6c7a7b0648883eeb18d9d998a32d1
Anleus <tomas.rui598@gmail.com>
2019-04-29 16:14:59 +0200

    GameManager creado

---
+++ greedy_game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public int score;
+    public int lifes;
+    public int lifeBar;
+
+    public bool playerDied;
+
+    void Awake()
+    {
+        MakeSingleton();
+    }
+
+    private void MakeSingleton()
+    {
+        if (instance != null) { 
+            Destroy(gameObject);
+        }
+        else { 
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }      
+    }
+}
