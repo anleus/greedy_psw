3e7d85bf9576298d160072375c551df8ff83c592
FVJohnny <beox12@gmail.com>
2019-05-06 16:09:33 +0200

    puto mono

---
+++ greedy_game/Assets/Scripts/Enemies/Base_enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base_enemy : Enemy { 
+
+
+    void Awake()
+    {
+        damageInflicted = 1;
+        speed = 175f;
+    }
+
+    protected override void Movement()
+    {
+        base.Movement(); //de momento
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "greedy")
+        {
+            collision.gameObject.GetComponent<Stats>().lifes -= damageInflicted;
+            Debug.Log("joder que daño");
+        }
+    }
+}
