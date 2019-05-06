92155f8d076e509817224f21863cb8a946987a40
crigasro <gasparrozalen@gmail.com>
2019-05-06 17:07:11 +0200

    Pantalla de Game over

--- greedy_game/Assets/Scripts/menu_scripts/MainMenu.cs
+++ greedy_game/Assets/Scripts/menu_scripts/MainMenu.cs
@@ -5,8 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    // Nota: este script se empleará para todas las pantallas que tengan menu
+    // (la principal, la de game over, etc)
     public void PlayGame()
     {
         SceneManager.LoadScene("Scenes/Map01");
+    }
+
+     public void QuitGame()
+    {
+        Debug.Log("Has salido del juego");
+        Application.Quit();
     }
 }
