bc3f7cab9546c4ac36ac5b3243441fd1ceef5f58
crigasro <gasparrozalen@gmail.com>
2019-05-12 17:09:59 +0200

    Commit

---
+++ Assets/Scripts/Menu/OptionsMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            Resolution currentRes = resolutions[i];
+            string option = currentRes.width + " x " + currentRes.height;
+            options.Add(option);
+
+            if(
+                currentRes.width == Screen.currentResolution.width 
+                && currentRes.height == Screen.currentResolution.height
+                )
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetVolume(float volume) 
+    {
+        audioMixer.SetFloat("masterVolume", volume);
+    }
+
+    public void SetFullscreen(bool isfullscreen)
+    {
+        Screen.fullScreen = isfullscreen;
+    }
+
+}
