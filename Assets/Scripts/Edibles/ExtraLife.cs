14ded12515fa065e1db550ae4141c6ef84d46a48
arnaupool <arnyr@hotmail.es>
2019-05-12 15:04:06 +0200

    Spawneación de la vida hecha, falta que se aumente en los stats del mono

---
+++ Assets/Scripts/Edibles/ExtraLife.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtraLife : BaseEntity
+{
+    protected override void OnCollision(Collision2D col)
+    {
+        GameManager.CreateEffect(effect, transform.position);
+        GameManager.CreateEffect(effect, col.gameObject.transform.position);
+        Destroy(gameObject);
+
+        //Hacer que aumente en 1 la vida
+        //GameManager.instance.playerStats.IncreaseCalories(bonusCalories);
+    }
+}
---
+++ Assets/Scripts/Edibles/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    //Límites para mapa pequeño:
+    //X[-5.5, 5.5]
+    //Y[-3.5, 3.5]
+
+
+    public GameObject vida;
+    private float time;
+    public Transform[] spawnpoints;
+
+    void Start()
+    {
+        time = Random.Range(7f, 15f);
+        Debug.Log("Next life coming in: " + time);
+        Invoke("spawnLife", time);
+    }
+
+    public void spawnLife()
+    {
+        int sp = Random.Range(0, spawnpoints.Length);
+        Instantiate(vida, spawnpoints[sp].position, Quaternion.identity);
+    }
+
+}
