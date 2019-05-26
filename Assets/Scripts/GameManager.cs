a86fad95a7105b293c43127511d75e92c984172f
FVJohnny <jofervac@gmail.com>
2019-05-26 13:05:26 +0200

    Corrección error null reference gamemanager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -213,9 +213,11 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        PlayerStats stats = getPlayerStats();
-        if (stats != null)
+        GameObject player = getPlayer();
+        PlayerStats stats;
+        if (player != null)
         {
+            stats = getPlayerStats();
             calories = stats.calories;
             damageReceived = stats.health;
             lifes = stats.lifes;
