769f7e434e21cf16a3ac21cc325752fd5bcdd192
arnaupool <arnyr@hotmail.es>
2019-05-30 01:10:30 +0200

    Arreglado otra vez el power-up

---
+++ Assets/Scripts/Powerups/StopPU.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopPU :  BasePowerup
+{
+    public GameObject powerupEffect;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            PickUp(powerupEffect, other, 2);
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<CircleCollider2D>().enabled = false;
+        }
+    }
+}
